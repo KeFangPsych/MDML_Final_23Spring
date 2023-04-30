08a3c9421310f5109bed76f16ea535261f3adaff
Ke Fang <121217651+KeFangPsych@users.noreply.github.com>
2023-04-30 18:23:31 -0400

    Updated data_retrieve
    Updated data_retrieve

--- scripts/data_retrieve.R
+++ scripts/data_retrieve.R
@@ -5,30 +5,20 @@ library(googledrive)
 # Authenticate with Google
 drive_auth()
 
-# Liberal media from facebook
-lib_media_fb_id <- "17dHtrwAGnNzcJTAFrb_WpCMyfPup3-cB"
-lib_media_fb <- drive_get(as_id(lib_media_fb_id))
+# Get the file ID from the file URL
 temp_file <- tempfile()
-drive_download(lib_media_fb, path = temp_file)
-lib_media_fb_data <- readRDS(temp_file)
+drive_download(drive_get(as_id("1hdZyGfpNsKPGUBXu8tTTocsVhceR2V7E")), 
+               path = temp_file)
+source(temp_file)
+
+# Liberal media from facebook
+# str(lib_media_fb_data)
 
 # Conservative media from facebook
-con_media_fb_id <- "1-KBo97SeLYIA28QL7SbMsGkCMUajg7Jh"
-con_media_fb <- drive_get(as_id(con_media_fb_id))
-temp_file <- tempfile()
-drive_download(con_media_fb, path = temp_file)
-con_media_fb_data <- readRDS(temp_file)
+# str(con_media_fb_data)
 
 # Liberal congressman/woman from facebook
-lib_congress_fb_id <- "1sIcpN2efXOMFDq6S7GomCS8WYhPN7X2T"
-lib_congress_fb <- drive_get(as_id(lib_congress_fb_id))
-temp_file <- tempfile()
-drive_download(lib_congress_fb, path = temp_file)
-lib_congress_fb_data <- readRDS(temp_file)
+# str(lib_congress_fb_data)
 
 # Conservative congressman/woman from facebook
-con_congress_fb_id <- "1qWMHc9qfKd-1__qOkjZMiFd705amn1mU"
-con_congress_fb <- drive_get(as_id(con_congress_fb_id))
-temp_file <- tempfile()
-drive_download(con_congress_fb, path = temp_file)
-con_congress_fb_data <- readRDS(temp_file)
+# str(con_congress_fb_data)
